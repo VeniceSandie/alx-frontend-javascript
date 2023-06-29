474eaf0674b3682ddf4ed2e9074fa2ebfe0ba5dc
VeniceSandie <sandisiwe2405@gmail.com>
2023-06-29 11:13:00 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,20 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+  ])
+    .then((results) => {
+      const arr = [];
+      for (const result of results) {
+        if (result.status === 'fulfilled') {
+          arr.push({ status: 'fulfilled', value: result.value });
+        } else {
+          arr.push({ status: 'rejected', reason: result.reason });
+        }
+      }
+      return arr;
+    })
+    .catch((error) => {
+      console.error('Error during profile signup:', error);
+      throw error;
+    });
 }
